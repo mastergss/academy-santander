588c2035251b030c5a52622583ef0b60e053899b
mastergss <gsosilva@mail.com>
2025-07-29 18:08:36 -0300

    estruturas de controle if-elif-else

---
+++ Estruturas_de_controle/estruturas_de_controle.py
@@ -0,0 +1,16 @@
+idade = 18
+
+if idade < 18:
+    print("Você é menor de idade.")
+
+elif idade >= 18 and idade < 60:
+    print("Você é maior de idade.")
+
+elif idade < 18:
+    print("Você é menor de idade.")
+
+elif idade == 60:
+    print("Você acabou de completar 60 anos.")
+
+else:
+    print("Você é um adulto.")
