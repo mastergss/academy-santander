71d25fe6beea2256abae8ea51cee9847bfc77ec0
mastergss <gsosilva@mail.com>
2025-07-30 19:35:10 -0300

    Estrutura de dados - Tuplas criar

---
+++ Estruturas_de_dados/Tuplas/tuplas.py
@@ -0,0 +1,4 @@
+pontos = (10, 20, 30, 40, 50)
+print(pontos[0])  # Acessando o primeiro elemento da tupla
+print(f"Segundo elemento: {pontos[1]}")  # Acessando o segundo elemento da tupla
+print(f"Último elemento: {pontos[-1]}")  # Acessando o último elemento da tupla
