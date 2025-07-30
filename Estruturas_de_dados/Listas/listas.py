6ee04e95cd48a90fe9d23fc22394d62ddefd26fb
mastergss <gsosilva@mail.com>
2025-07-30 18:44:52 -0300

    Estrutura de dados - Listas criar

---
+++ Estruturas_de_dados/Listas/listas.py
@@ -0,0 +1,11 @@
+frutas = ["maçã", "banana", "laranja", "uva"]
+print(frutas[0])  # Acessa o primeiro elemento da lista
+print(frutas[1])  # Acessa o segundo elemento da lista
+print(frutas[2])  # Acessa o terceiro elemento da lista
+print(frutas[3])  # Acessa o quarto elemento da lista
+
+print(f"Imprimir a lista de {frutas} do fim para o começo:")
+print(frutas[-1])  # Acessa o último elemento da lista
+print(frutas[-2])  # Acessa o penúltimo elemento da lista
+print(frutas[-3])  # Acessa o antepenúltimo elemento da lista
+print(frutas[-4])  # Acessa o quarto elemento da lista a partir do fim
