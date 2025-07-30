3121f8d6c9f4a4d7bfe4ba639be2b0eacae728d8
mastergss <gsosilva@mail.com>
2025-07-30 12:59:08 -0300

    loops com operacao matematica de mutiplicação

---
+++ loops/for_multiplicar.py
@@ -0,0 +1,14 @@
+"""função range para gerar números de 1 a 10;
+utilizar loop for para multiplicar por 5
+impressão simples do numero multiplicado por 5;"""
+print("Multiplicar números de 1 a 10 por 5:")
+for numero in range(1, 11):
+    print(numero * 5)
+
+# Utilizar loop for para multiplicar números de 1 a 10 por 5
+# utilizar variável resultado para armazenar o valor da multiplicação
+# imprimir o resultado da multiplicação
+print("\nMultiplicação de números de 1 a 10 por 5:")
+for numero in range(1, 11):
+    resultado = numero * 5
+    print(f"{numero} * 5 = {resultado}")
---
+++ loops/while_multiplicar.py
@@ -0,0 +1,6 @@
+print("Números de 1 a 10 multiplicados por 5:")
+contador = 1
+while contador <= 10:
+    resultado = contador * 5
+    print(f"{contador} * 5 = {resultado}")
+    contador += 1
