8e46591c3f28fe13281df5b374e4c902d02ad0b9
mastergss <gsosilva@mail.com>
2025-07-30 13:20:54 -0300

    loops em estruturas de controle

---
+++ Estruturas_de_controle/controle de loops/continue.py
@@ -0,0 +1,4 @@
+for numero in range(10):
+    if numero % 2 == 0:
+        continue
+    print(f"{numero} é ímpar, continuando o loop.")
---
+++ Estruturas_de_controle/controle de loops/pass.py
@@ -0,0 +1,3 @@
+for numero in range(5):
+    pass  # Esta linha não faz nada, é apenas um marcador de posição
+# Exemplo de uso de 'pass' em uma estrutura de controle
